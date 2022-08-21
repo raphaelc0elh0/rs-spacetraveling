4798f59d860b4f0fa5709af1bde70bacddb28f7a
Raphael Coelho <raphael_coelho@hotmail.com.br>
2022-08-20 22:45:58 -0300

    feat: add getStaticProps in main page to retrieve posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import Link from 'next/link';
 import { FiCalendar, FiUser } from 'react-icons/fi';
-
-// import { getPrismicClient } from '../services/prismic';
+import { getPrismicClient } from '../services/prismic';
+import format from '../utils/format';
 
 // import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
@@ -26,64 +27,62 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home() {
+export default function Home({ postsPagination }: HomeProps) {
+  const posts = postsPagination.results;
+
   return (
     <>
       <Head>
         <title>Home | spacetraveling</title>
       </Head>
       <main className={styles.postsContainer}>
-        <Link href={`/`}>
-          <a>
-            <strong>Como utilizar Hooks</strong>
-            <p>Pensando em sincronização em vez de cliclos de vida.</p>
-            <div>
-              <span>
-                <FiCalendar /> 15 Mar 2021
-              </span>
-              <span>
-                <FiUser /> Joseph Oliveira
-              </span>
-            </div>
-          </a>
-        </Link>
-        <Link href={`/`}>
-          <a>
-            <strong>Como utilizar Hooks</strong>
-            <p>Pensando em sincronização em vez de cliclos de vida.</p>
-            <div>
-              <span>
-                <FiCalendar /> 15 Mar 2021
-              </span>
-              <span>
-                <FiUser /> Joseph Oliveira
-              </span>
-            </div>
-          </a>
-        </Link>
-        <Link href={`/`}>
-          <a>
-            <strong>Como utilizar Hooks</strong>
-            <p>Pensando em sincronização em vez de cliclos de vida.</p>
-            <div>
-              <span>
-                <FiCalendar /> 15 Mar 2021
-              </span>
-              <span>
-                <FiUser /> Joseph Oliveira
-              </span>
-            </div>
-          </a>
-        </Link>
-        <button type="button">Carregar mais posts</button>
+        {posts.map(post => (
+          <Link key={post.uid} href={`/post/${post.uid}`}>
+            <a>
+              <strong>{post.data.title}</strong>
+              <p>{post.data.subtitle}</p>
+              <div>
+                <span>
+                  <FiCalendar /> {post.first_publication_date}
+                </span>
+                <span>
+                  <FiUser /> {post.data.author}
+                </span>
+              </div>
+            </a>
+          </Link>
+        ))}
+        {postsPagination.next_page && (
+          <button type="button">Carregar mais posts</button>
+        )}
       </main>
     </>
   );
 }
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient({});
-//   // const postsResponse = await prismic.getByType(TODO);
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient({});
+  const response = await prismic.getByType('post', {
+    fetch: ['post.title', 'post.subtitle', , 'post.author'],
+    pageSize: 2,
+  });
 
-//   // TODO
-// };
+  const posts = response.results.map(post => ({
+    uid: post.uid,
+    first_publication_date: format(post.first_publication_date),
+    data: {
+      title: post.data.title,
+      subtitle: post.data.subtitle,
+      author: post.data.author,
+    },
+  }));
+
+  return {
+    props: {
+      postsPagination: {
+        next_page: response.next_page,
+        results: posts,
+      },
+    },
+  };
+};
--- src/utils/format.ts
+++ src/utils/format.ts
@@ -2,7 +2,7 @@ import { format as f } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 
 export default function format(date: string) {
-  return f(new Date(date), 'dd MM yyyy', {
+  return f(new Date(date), 'dd MMM yyyy', {
     locale: ptBR,
   });
 }
